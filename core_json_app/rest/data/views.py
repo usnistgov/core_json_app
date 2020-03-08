5b0ea3b67b52cbf51eb93c0e9fe0ab947a41c2fd
adilmania <adil.mania.pro@gmail.com>
2020-03-08 21:48:00 +0100

    add data get

--- core_json_app/rest/data/views.py
+++ core_json_app/rest/data/views.py
@@ -4,7 +4,7 @@
 from django.http import Http404
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -18,6 +18,59 @@ class DataList(APIView):
     """ List all user Data, or create a new one.
     """
     permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        """ Get all user Data
+
+        Url Parameters:
+
+            template: template_id
+            title: document_title
+
+        Examples:
+
+            ../data/
+            ../data?workspace=[workspace_id]
+            ../data?template=[template_id]
+            ../data?title=[document_title]
+            ../data?template=[template_id]&title=[document_title]
+
+        Args:
+
+            request: HTTP request
+
+        Returns:
+
+            - code: 200
+              content: List of data
+            - code: 500
+              content: Internal server error
+        """
+        try:
+            # Get object
+            data_object_list = data_api.get_all_by_user(request.user)
+
+            # Apply filters
+            workspace = self.request.query_params.get('workspace', None)
+            if workspace is not None:
+                data_object_list = data_object_list.filter(workspace=workspace)
+
+            template = self.request.query_params.get('template', None)
+            if template is not None:
+                data_object_list = data_object_list.filter(template=template)
+
+            title = self.request.query_params.get('title', None)
+            if title is not None:
+                data_object_list = data_object_list.filter(title=title)
+
+            # Serialize object
+            data_serializer = DataSerializer(data_object_list, many=True)
+
+            # Return response
+            return Response(data_serializer.data, status=status.HTTP_200_OK)
+        except Exception as api_exception:
+            content = {'message': str(api_exception)}
+            return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request):
         """ Create a Data
@@ -64,6 +117,146 @@ class DataList(APIView):
         except Exception as api_exception:
             content = {'message': str(api_exception)}
             return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class DataDetail(APIView):
+    """ Retrieve, update or delete a Data
+    """
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_object(self, request, pk):
+        """ Get data from db
+
+        Args:
+
+            request: HTTP request
+            pk: ObjectId
+
+        Returns:
+
+            Data
+        """
+        try:
+            return data_api.get_by_id(pk, request.user)
+        except exceptions.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """ Retrieve a data
+
+        Args:
+
+            request: HTTP request
+            pk: ObjectId
+
+        Returns:
+
+            - code: 200
+              content: Data
+            - code: 404
+              content: Object was not found
+            - code: 500
+              content: Internal server error
+        """
+        try:
+            # Get object
+            data_object = self.get_object(request, pk)
+
+            # Serialize object
+            serializer = DataSerializer(data_object)
+
+            # Return response
+            return Response(serializer.data)
+        except Http404:
+            content = {'message': 'Data not found.'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        except Exception as api_exception:
+            content = {'message': str(api_exception)}
+            return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, pk):
+        """ Delete a Data
+
+        Args:
+
+            request: HTTP request
+            pk: ObjectId
+
+        Returns:
+
+            - code: 204
+              content: Deletion succeed
+            - code: 404
+              content: Object was not found
+            - code: 500
+              content: Internal server error
+        """
+        try:
+            # Get object
+            data_object = self.get_object(request, pk)
+
+            # delete object
+            data_api.delete(data_object, request.user)
+
+            # Return response
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Http404:
+            content = {'message': 'Data not found.'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        except Exception as api_exception:
+            content = {'message': str(api_exception)}
+            return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def patch(self, request, pk):
+        """ Update a Data
+
+        Parameters:
+
+            {
+                "title": "new_title",
+                "dict_content": "new_dict_content"
+            }
+
+        Args:
+
+            request: HTTP request
+            pk: ObjectId
+
+        Returns:
+
+            - code: 200
+              content: Updated data
+            - code: 400
+              content: Validation error
+            - code: 404
+              content: Object was not found
+            - code: 500
+              content: Internal server error
+        """
+        try:
+            # Get object
+            data_object = self.get_object(request, pk)
+
+            # Build serializer
+            data_serializer = DataSerializer(instance=data_object,
+                                             data=request.data,
+                                             partial=True)
+
+            # Validate data
+            data_serializer.is_valid(True)
+            # Save data
+            data_serializer.save(user=request.user)
+
+            return Response(data_serializer.data, status=status.HTTP_200_OK)
+        except ValidationError as validation_exception:
+            content = {'message': validation_exception.detail}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+            content = {'message': 'Data not found.'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        except Exception as api_exception:
+            content = {'message': str(api_exception)}
+            return Response(content, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class DataDownload(APIView):
     """ Download JSON file in data
